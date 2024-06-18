bf14ab059a1a7420be6e6c62c3efbe06e0b8047c
Moa Winberg <moa.winberg@icloud.com>
2024-06-18 20:30:12 +0200

    add icon and modal on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,120 @@
-import { useState } from "react";
-import "./App.css";
-import { MyComponent } from "./components/MyComponent";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+import React, { MouseEvent, useState, useEffect } from "react";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { Modal, Button, TextField, Box, IconButton } from "@mui/material";
 
-const queryClient = new QueryClient();
+type Icon = {
+  xPosition: number;
+  yPosition: number;
+  modalOpen: boolean;
+  comment?: string;
+  userName?: string;
+};
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  const theme = createTheme({
-    palette: {
-      mode: darkMode ? "dark" : "light",
-    },
-  });
+  const [icons, setIcons] = useState<Icon[]>([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(icons);
+  }, [icons]);
+
+  const handleClose = (iconIndex: number) => {
+    setIcons((prevIcons) =>
+      prevIcons.map((icon, index) =>
+        index === iconIndex ? { ...icon, modalOpen: false } : icon
+      )
+    );
+  };
+
+  const showComment = (iconIndex: number) => {
+    setModalIsOpen(true);
+    setIcons((prevIcons) =>
+      prevIcons.map((icon, index) =>
+        index === iconIndex ? { ...icon, modalOpen: true } : icon
+      )
+    );
+  };
+
+  const addComment = (iconIndex: number) => {
+    console.log("add comment");
+  };
+
+  const handleClickOnScreen = (xPosition: number, yPosition: number) => {
+    // if a modal is open, we should not add a new icon
+    if (modalIsOpen) {
+      setModalIsOpen(false);
+      return;
+    }
+
+    const xPercent = (xPosition / window.innerWidth) * 100;
+    const yPercent = (yPosition / window.innerHeight) * 100;
+
+    const iconIndex = icons.findIndex(
+      (icon) =>
+        Math.abs(icon.xPosition - xPercent) <= 2 &&
+        Math.abs(icon.yPosition - yPercent) <= 2
+    );
+
+    if (iconIndex !== -1) {
+      showComment(iconIndex);
+      // if (icons[iconIndex].comment) {
+      //   showComment(iconIndex);
+      // }
+      // addComment(iconIndex);
+    } else {
+      const newIcon = {
+        xPosition: xPercent,
+        yPosition: yPercent,
+        modalOpen: false,
+      };
+      setIcons((prevIcons) => [...prevIcons, newIcon]);
+    }
+  };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-          }}
-        >
-          <IconButton onClick={() => setDarkMode(!darkMode)} color="inherit">
-            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+    <div
+      onClick={(e: MouseEvent<HTMLDivElement>) =>
+        handleClickOnScreen(e.clientX - 15, e.clientY - 15)
+      }
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      {icons.map((icon, index) => (
+        <React.Fragment key={index}>
+          <IconButton
+            style={{
+              position: "absolute",
+              top: `${icon.yPosition}%`,
+              left: `${icon.xPosition}%`,
+            }}
+            color="inherit"
+          >
+            <ChatBubbleIcon />
           </IconButton>
-        </Box>
-        <MyComponent />
-      </ThemeProvider>
-    </QueryClientProvider>
+          <Modal
+            style={{
+              position: "absolute",
+              top: `${icon.yPosition}%`,
+              left: `${icon.xPosition + 5}%`,
+            }}
+            open={icon.modalOpen}
+            onClose={() => handleClose(index)}
+          >
+            <Box
+              sx={{
+                background: "white",
+                width: 300,
+                height: 100,
+                borderRadius: 2,
+                display: "flex",
+                padding: 2,
+              }}
+            >
+              Moa Winberg: {icon.comment}
+            </Box>
+          </Modal>
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
 
